b70c4938fbb46fe562ada5eec31210f769b3df13
Ashwin Patthi <ashwin@mbpro.local>
2014-11-07 22:59:56 -0500

    adding some file missed in last commit

---
+++ activateuser.php
@@ -0,0 +1,6 @@
+<?php
+
+	include_once 'includes/functions.php';
+
+	echo activateUser($_GET['id']);
+?>
---
+++ includes/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+// define('HOST_URL', 'http://tundra.csd.sc.edu/manuscriptlink_prod');
+define('HOST_URL', 'http://localhost/manuscriptlink_prod');
+define('WEBMAIL_ID', 'admin@manuscriptlink.com');
+DEFINE('HTTP_TYPE', 'http');
+DEFINE('HTTP_ROOT', $_SERVER['HTTP_HOST']);
+DEFINE('HTTP_FOLDER', dirname($_SERVER['PHP_SELF']) . '/');
+DEFINE('BASE_URL', HTTP_TYPE . "://" . HTTP_ROOT . HTTP_FOLDER);
+
+?>
